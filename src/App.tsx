ff1a7d0e009a28153509cfc02a4a41081e4be36c
pablodevv <79975765+pablodevv@users.noreply.github.com>
2025-04-05 16:20:27 -0300

    tyt

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Quiz from './pages/Quiz';
 import Summary from './pages/Summary';
-import Checkout from './pages/checkout';
+import Checkout from './pages/Checkout';
 
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/quiz" element={<Quiz />} />
         <Route path="/summary" element={<Summary />} />
-        <Route path="/checkout" element={<checkout />} />
+        <Route path="/checkout" element={<Checkout />} />
       </Routes>
     </Router>
   );
