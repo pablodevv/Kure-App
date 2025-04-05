c220c5df29d7d80aeda15f15c056d5e3ac39186f
pablodevv <79975765+pablodevv@users.noreply.github.com>
2025-04-05 13:24:46 -0300

    checkout primeiro

---
+++ src/pages/Checkout.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Checkout() {
+  return (
+    <div className="min-h-screen bg-[#0A061E] text-white flex items-center justify-center">
+      <h1 className="text-2xl">Página de Checkout em construção...</h1>
+    </div>
+  );
+}
