e9a0d07a591e9fd0b104f37644cd3663cdaf7df7
pablodevv <79975765+pablodevv@users.noreply.github.com>
2025-04-05 15:31:18 -0300

    fonte

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        serif: ['Georgia', 'serif'],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
