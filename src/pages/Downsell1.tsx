c283af671a8b5f742a4836c4f542c4c775a38306
pablodevv <79975765+pablodevv@users.noreply.github.com>
2025-04-14 10:47:48 -0300

    Update Downsell1.tsx

--- src/pages/Downsell1.tsx
+++ src/pages/Downsell1.tsx
@@ -3,6 +3,12 @@ import { Button } from '../components/Button';
 import { Flame, Gift, TimerReset, CheckCircle, UserCircle2 } from "lucide-react"
 import { useNavigate } from "react-router-dom"
 
+useEffect(() => {
+  // Rola a página para o topo sempre que ela for renderizada
+  window.scrollTo(0, 0);
+}, []);
+
+
 export default function DownsellRenovacao3Meses() {
   const [timeLeft, setTimeLeft] = useState(600) // 10 minutos
   const navigate = useNavigate()
