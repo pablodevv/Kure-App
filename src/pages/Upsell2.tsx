a945955ac97faf7c878e82aded66dec866f221bb
pablodevv <79975765+pablodevv@users.noreply.github.com>
2025-04-14 10:55:01 -0300

    Update Upsell2.tsx

--- src/pages/Upsell2.tsx
+++ src/pages/Upsell2.tsx
@@ -3,15 +3,19 @@ import { Button } from '../components/Button'
 import { CheckCircle, Flame, TimerReset, Users, Star, Gift } from "lucide-react"
 import { useNavigate } from "react-router-dom"
 
-useEffect(() => {
-  // Rola a página para o topo sempre que ela for renderizada
-  window.scrollTo(0, 0);
-}, []);
 
 export default function UpsellPlano6Meses() {
   const [timeLeft, setTimeLeft] = useState(600)
   const navigate = useNavigate()
 
+
+
+useEffect(() => {
+  // Rola a página para o topo sempre que ela for renderizada
+  window.scrollTo(0, 0);
+}, []);
+
+  
   useEffect(() => {
     const interval = setInterval(() => {
       setTimeLeft((prev) => (prev > 0 ? prev - 1 : 0))
