61230f51a3cdae17f1ed6a401d7d201bef5cacf3
pablodevv <79975765+pablodevv@users.noreply.github.com>
2025-04-14 00:36:45 -0300

    timer color

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -358,7 +358,7 @@ const handleGetBottomPlan = () => {
 
   return (
     <div className="min-h-screen bg-[#0F0A3C] text-white pt-16">
-  <div className="fixed top-0 w-full bg-[#FF4B7B] py-4 text-center z-50">
+  <div className="fixed top-0 w-full py-4 text-center z-50" style={{ backgroundColor: '#ce5561' }}>
     <p>
       Comece sua primeira sessão {getTimeOfDayMessage()}!<br />Seu desconto termina em:{' '}
       <span className="font-bold">
