b11208121d64f2bde0a794a744e2ddff27edb894
pablodevv <79975765+pablodevv@users.noreply.github.com>
2025-04-14 02:20:48 -0300

    Update Upsell2.tsx

--- src/pages/Upsell2.tsx
+++ src/pages/Upsell2.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Button } from 'react-bootstrap';
+import { Button } from '../components/Button';
 
 const UpsellPage = () => {
   // Timer para urgência
